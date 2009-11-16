d925b050b59d7148fd2010ca8116d09f4abc52a1
Aaron Barnes <aaron.barnes@hbcosmo.com>
2009-11-16 13:28:50 +1300

    www/header.tpl.php: Adding some basic styling

--- www/header.tpl.php
+++ www/header.tpl.php
@@ -1,6 +1,25 @@
 <html>
 <head>
     <title>Iron</title>
+
+    <style>
+        body, h1, h2
+        {
+            font-family: Verdana, "sans-serif";
+        }
+
+        body, td
+        {
+            font-size: 80%;
+        }
+
+        #header
+        {
+            padding-bottom: 0.5em;
+            margin-bottom: 1em;
+            border-bottom: 1px solid #ccc;
+        }
+    </style>
 </head>
 
 <body>
