a5c74413b683264cb0455c548cc4a8407819f00e
Aaron Barnes <aaronb@catalyst.net.nz>
2010-04-19 12:42:47 +1200

    public/js/global.js: Bind dialog to children's add buttons

--- public/js/global.js
+++ public/js/global.js
@@ -1,14 +1,18 @@
 $(function() {
 
-    $('a.open-dialog').fancybox({
-        ajax: {
+    var fancybox_config = {
+          ajax: {
             type: 'GET'
-        },
-        padding: 20,
-        width: 640,
-        height: 480,
-    });
+        }
+        , padding: 20
+        , width: 640
+        , height: 480
+    };
 
+    // Bind dialog to links
+    $('a.open-dialog').fancybox(fancybox_config);
+
+    // Expand parent and load children on click
     $('a.parent').live('click', function(e) {
 
         e.preventDefault();
@@ -25,7 +29,13 @@ $(function() {
         // Otherwise, create a new one and insert ajax result
         var children = $('<div class="children"></div>');
         children.load(link.attr('href'), function() {
+            // On load complete
+
+            // Append children to parent paragraph
             para.append(children);
+
+            // Bind dialog to links
+            $('a.open-dialog', children).fancybox(fancybox_config);
         });
 
     });
